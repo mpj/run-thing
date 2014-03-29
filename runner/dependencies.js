603d1ca963e35cd81dda97f6f8626d330cc8fbaa
Mattias Petter Johansson <mpj@spotify.com>
2014-03-29 16:12:23 +0100

    wip checkModule visualization

--- runner/dependencies.js
+++ runner/dependencies.js
@@ -1,3 +1,4 @@
 createBus = require('bus-thing')
-spec = require('spec-thing')
+spec = require('spec-thing').spec
+checkModule = require('spec-thing').checkModule
 pretty = require('../pretty-thing')
